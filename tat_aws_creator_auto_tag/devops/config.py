871331a335ca016706b085f6c2897f7f8a434139
Sanhe Hu <husanhe@gmail.com>
2019-10-26 23:17:30 -0400

    update config and serverless file

--- tat_aws_creator_auto_tag/devops/config.py
+++ tat_aws_creator_auto_tag/devops/config.py
@@ -24,6 +24,8 @@ class Config(ConfigClass):
     def get_ENVIRONMENT_NAME(self):
         return "{}-{}".format(self.PROJECT_NAME_SLUG.get_value(self), self.STAGE.get_value())
 
+    AWS_ACCOUNT_ID = Constant()
+    AWS_REGION = Constant()
     AWS_PROFILE_FOR_DEPLOY = Constant()
 
     AWS_PROFILE_FOR_BOTO3 = Derivable()
@@ -36,3 +38,40 @@ class Config(ConfigClass):
             return self.AWS_PROFILE_FOR_DEPLOY.get_value()
 
     S3_BUCKET_FOR_DEPLOY = Constant()
+
+    S3_BUCKET_FOR_CLOUDTRAIL = Constant()
+
+    LAMBDA_ENV_VAR_PREFIX = Constant(default="TAT_AWS_CREATOR_AUTO_TAG_")
+
+    IAM_ROLE_NAME = Constant()
+
+    IAM_ROLE_ARN = Derivable()
+
+    @IAM_ROLE_ARN.getter
+    def get_IAM_ROLE_ARN(self):
+        return "arn:aws:iam::{}:role/{}".format(
+            self.AWS_ACCOUNT_ID.get_value(),
+            self.IAM_ROLE_NAME.get_value(),
+        )
+
+    LATEST_LAYER_ARN = Derivable()
+
+    @LATEST_LAYER_ARN.getter
+    def get_LATEST_LAYER_ARN(self):
+        """
+        """
+        import boto3
+
+        boto_ses = boto3.Session(profile_name=self.AWS_PROFILE_FOR_BOTO3.get_value())
+        lbd_client = boto_ses.client("lambda")
+        res_list_layers = lbd_client.list_layers(
+            CompatibleRuntime="python3.6"
+        )
+        latest_layer_arn = None
+        for layer_data in res_list_layers["Layers"]:
+            layer_name = layer_data["LayerName"]
+            if layer_name == self.PROJECT_NAME.get_value():
+                latest_layer_arn = layer_data["LatestMatchingVersion"]["LayerVersionArn"]
+        if latest_layer_arn is None:
+            raise ValueError
+        return latest_layer_arn
--- tat_aws_creator_auto_tag/devops/config_init.py
+++ tat_aws_creator_auto_tag/devops/config_init.py
@@ -1,9 +1,14 @@
 # -*- coding: utf-8 -*-
 
-from .config import Config
+import os
+import json
+from tat_aws_creator_auto_tag.devops.config import Config
 
 config = Config()
 if config.is_aws_lambda_runtime():
-    pass
+    config.update_from_env_var(prefix=config.LAMBDA_ENV_VAR_PREFIX.get_value())
 else:
     config.update_from_raw_json_file()
+    with open(os.path.join(config.CONFIG_DIR, "00-config-shared.json"), "rb") as f:
+        config.update(json.loads(f.read().decode("utf-8")))
+    config.dump_python_json_config_file()
